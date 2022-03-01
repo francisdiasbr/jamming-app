9ca995833f34e6c31c547173124e43ead5526239
Francis <francisdiasbr@gmail.com>
2022-02-28 23:37:11 -0300

    feat: Salva a playlist criada

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,6 @@
 const clientId = '979a80c6dd4b4bd8a170d6e35438083a';
 const redirectUri = 'http://localhost:3000/';
-let accessToken;
+let accessToken = 'BQCj6-DHzeMFiVOVW3QtUgSBWRomv0SuQ2LVG6dqS2BiPDNSZF-CzNvmSnWmQ5NpEke-Jit3XlYzaR3WvNhwDbWAP2PJusIdyBYdPjeCRW2_NsvL95O1dglEJ4XOKous0C8CHY-79-qmEUr256ODjoxT_XdfhTXelqnR9OFoAnxoxU7c';
 
 const Spotify = {
   getAccessToken() {
@@ -20,12 +20,14 @@ const Spotify = {
       return accessToken;
     } else {
       const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
+      console.log(accessUrl);
       window.location = accessUrl;
     }
   },
 
-  search(term) {
-    const accessToken = Spotify.getAccessToken;
+  async search(term) {
+    const accessToken = Spotify.getAccessToken();
+    console.log(accessToken);
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`
@@ -45,6 +47,34 @@ const Spotify = {
       }));
     });
   },
+
+  savePlaylist(name, trackUris) {
+    if (!name || !trackUris.length) {
+      return;
+    }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+    let userId;
+    const url = 'https://api.spotify.com/v1/me'
+    return fetch(url, { headers: headers }).then(response => response.json()).then(jsonResponse => {
+      userId = jsonResponse.id;
+      return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,
+      {
+        headers: headers,
+        method: 'POST',
+        body: JSON.stringify({ name: name })
+      }).then(response => response.json()
+      ).then(jsonResponse => {
+        const playlistId = jsonResponse.id;
+        return fetch(`/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+          headers: headers,
+          method: 'POST',
+          body: JSON.stringify({ uris: trackUris }),
+        })
+      })
+  })
+}
 }
 
 export default Spotify;
